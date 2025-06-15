38c9375a1d0627dca0ef4c2f7cbad8797a1dd04a
Kecci Kun <abyan.kecci@stockbit.com>
2025-06-16 01:18:10 +0700

    enhance github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/agb/', // Replace 'your-repo-name' with your actual GitHub repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
